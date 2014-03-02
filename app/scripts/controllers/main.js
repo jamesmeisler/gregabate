d2949688457ebd9e4742ae9524a75cac6d8ae522
James Meisler <james@mindfulware.com>
2014-03-01 22:36:31 -0600

    Ticket #1 - Angular setup

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,4 +7,22 @@ angular.module('abateApp')
       'AngularJS',
       'Karma'
     ];
+
   });
+
+
+   function CarouselDemoCtrl($scope) {
+       $scope.myInterval = 5000;
+       var slides = $scope.slides = [];
+       $scope.addSlide = function() {
+       var newWidth = 960 + slides.length;
+       slides.push({
+	     image: 'http://lorempixel.com/'+ newWidth +'/480/nightlife/',
+	     text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+           ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+        });
+      };
+      for (var i=0; i<4; i++) {
+	    $scope.addSlide();
+      }
+	}
